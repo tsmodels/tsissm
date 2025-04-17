2ab6e9b27623190a941220282281d0866a766ddd
Alexios Galanos <alexios@4dscape.com>
2025-04-17 09:50:20 +0300

    fix to testing

--- tests/testthat/helper-global.R
+++ tests/testthat/helper-global.R
@@ -14,11 +14,10 @@ dynamic_benchmark <-  c("alpha" = 0.2794620879, "beta" = 0.0009843874, "gamma12.
                         "gamma12.2" = -0.0009547473, "eta" = 0.1366928283, "delta" = 0.8237582308,
                         "LogLik" = -4270.23)
 
-spec_bc_benchmark <- issm_modelspec(y, slope = TRUE, seasonal = TRUE, seasonal_frequency = 12, seasonal_harmonics = 5, lambda = NA)
-spec_bc_benchmark$parmatrix[parameters == "lambda", initial := 0.1773974]
+spec_bc_benchmark <- issm_modelspec(y, slope = TRUE, seasonal = TRUE, seasonal_frequency = 12, seasonal_harmonics = 5, lambda = 0.25)
 mod_bc_benchmark <- estimate(spec_bc_benchmark)
-bc_benchmark <-  c("alpha" = 4.034174894e-01, "beta" = 4.010118934e-08, "gamma12.1" = -1.083547953e-02, 
-                   "gamma12.2" = 1.444199458e-02, lambda = 1.272051337e-01, "sigma" = 0.1349176, "LogLik" = -4165.158)
+bc_benchmark <-  c("alpha" = 0.400342866, "beta" = 0.003315568, "gamma12.1" = -0.006163750, 
+                   "gamma12.2" = 0.001806082, "sigma" = 0.6742348, "LogLik" = -4186.793)
 
 spec_auto <- issm_modelspec(y, auto = TRUE, slope = c(TRUE, FALSE), seasonal = TRUE, seasonal_frequency = 12, seasonal_harmonics = list(c(5)), top_n = 2)
 mod_auto <- estimate(spec_auto)
